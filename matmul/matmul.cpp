2ba6aa62ce469d7e82b63f84942115d6630b0d96
AlexSaalbergCP <asaalber@calpoly.edu>
2016-11-14 19:47:50 -0800

    Changed matmul.cpp to use column-major for 2nd matrix

--- matmul/matmul.cpp
+++ matmul/matmul.cpp
@@ -8,13 +8,13 @@ void matmul( float*, const float*, const float*, unsigned int, unsigned int, uns
 ////////////////////////////////////////////////////////////////////////////////
 //! C = A * B
 //! @param C          result matrix
-//! @param A          matrix A 
+//! @param A          matrix A
 //! @param B          matrix B
 //! @param hA         height of matrix A
 //! @param wB         width of matrix B
 ////////////////////////////////////////////////////////////////////////////////
 
-void matmul(float* C, const float* A, const float* B, unsigned int hA, 
+void matmul(float* C, const float* A, const float* B, unsigned int hA,
     unsigned int wA, unsigned int wB)
 {
   for (unsigned int i = 0; i < hA; ++i)
@@ -22,7 +22,7 @@ void matmul(float* C, const float* A, const float* B, unsigned int hA,
       double sum = 0;
       for (unsigned int k = 0; k < wA; ++k) {
         double a = A[i * wA + k];
-        double b = B[k * wB + j];
+        double b = B[j * wB + k];
         sum += a * b;
       }
       C[i * wB + j] = (float)sum;
@@ -40,11 +40,19 @@ Matrix Allocate2ndMatrix(int height, int width)
 
   M.elements = (float*) malloc(size*sizeof(float));
 
+    for(unsigned int i = 0; i < M.width; i++) {
+        for(unsigned int j = 0; j < M.height; j++) {
+            M.elements[i + M.width*j] = (rand() / (float)RAND_MAX);
+        }
+    }
+
+
+  /*
   // this is a row-major allocation and initialization
   for(unsigned int i = 0; i < M.height * M.width; i++)
   {
     M.elements[i] = (rand() / (float)RAND_MAX);
   }
-  return M;
-}	
-
+    */
+return M;
+}
