b1fc38be27b5f5b5a59a78b9533e4ba291e7840c
AlexSaalbergCP <asaalber@calpoly.edu>
2016-11-16 00:25:46 -0800

    Optimized structs.cpp

--- structs/structs.cpp
+++ structs/structs.cpp
@@ -15,9 +15,9 @@
 
 // You'll need to modifiy this struct
 typedef struct nums {
-  double mydouble;
-  float myfloat;
-  long long int mylonglong;
+  double *mydouble;
+  float *myfloat;
+  long long int *mylonglong;
 } myNums;
 
 void init_nums(myNums *);
@@ -29,27 +29,35 @@ void compute_averages(myNums *);
 int main(int argc, char** argv) {
 
   // Data structure for the program
-  myNums *AoS;  // AoS will be an array of structs
+  myNums *SoA;  // AoS will be an array of structs
                 // You'll want to convert to a struct of arrays
 
   // Allocate and initialize the array of structs
   // This will also need to change
-  AoS  = (myNums *) malloc(NUMVALUES*sizeof(myNums));
+  SoA  = (myNums *) malloc(sizeof(myNums));
+  SoA->mydouble = (double *) malloc(NUMVALUES * sizeof(double));
+  SoA->myfloat = (float *) malloc(NUMVALUES * sizeof(float));
+  SoA->mylonglong = (long long int *) malloc(NUMVALUES * sizeof(long long int));
+
+
 
   hrt_start();
-  init_nums(AoS);  // time the initialization
+  init_nums(SoA);  // time the initialization
   hrt_stop();
   fprintf(stdout, "Initialization time of three %d vectors took %s.\n",
       NUMVALUES, hrt_string());
   
   hrt_start();
-  compute_averages(AoS);  // time the computation
+  compute_averages(SoA);  // time the computation
   hrt_stop();
   fprintf(stdout, "Computation time of three %d vectors took %s.\n",
       NUMVALUES, hrt_string());
 
   // Free 
-  free(AoS);
+  free(SoA->mydouble);
+  free(SoA->myfloat);
+  free(SoA->mylonglong);
+  free(SoA);
   return 0;
 }
 
@@ -57,14 +65,14 @@ int main(int argc, char** argv) {
 // You'll need to modify the following two functions to adjust how
 // the data structure is accessed. 
 
-void init_nums (myNums *AoS) {
+void init_nums (myNums *SoA) {
   unsigned int fseed = 2016;
   unsigned int dseed = 2017;
 
   for (unsigned int i = 0; i<NUMVALUES; ++i) {
-    AoS[i].myfloat = (rand_r(&fseed) / (float)RAND_MAX);
-    AoS[i].mydouble = (rand_r(&dseed) / (double)RAND_MAX);
-    AoS[i].mylonglong = (rand_r(&dseed) / (long long int)RAND_MAX);
+    SoA->myfloat[i] = (rand_r(&fseed) / (float)RAND_MAX);
+    SoA->mydouble[i] = (rand_r(&dseed) / (double)RAND_MAX);
+    SoA->mylonglong[i] = (rand_r(&dseed) / (long long int)RAND_MAX);
   }
 }
 
@@ -74,18 +82,18 @@ void compute_averages (myNums *AoS) {
   long long int sumll = 0;
 
   for (unsigned int i = 0; i<NUMVALUES; ++i) {
-    sumf += AoS[i].myfloat; 
+    sumf += AoS->myfloat[i]; 
   }
   fprintf(stdout, "Average of %d floats = %.5f.\n", 
       NUMVALUES, sumf/(float)NUMVALUES);
 
   for (unsigned int i = 0; i<NUMVALUES; ++i) {
-    sumd += AoS[i].mydouble; 
+    sumd += AoS->mydouble[i]; 
   }
   fprintf(stdout, "Average of %d doubles= %.5lf.\n", 
       NUMVALUES, sumf/(double)NUMVALUES);
   for (unsigned int i = 0; i<NUMVALUES; ++i) {
-    sumll += AoS[i].mylonglong; 
+    sumll += AoS->mylonglong[i]; 
   }
   fprintf(stdout, "Average of %d long long ints= %lld.\n", 
       NUMVALUES, sumll/(long long int)NUMVALUES);
